38a718015c44c670e41271fa128bfbc479cb09b6
Gerald Andrews <gerald2399@comcast.net>
2023-12-09 21:36:24 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,235 @@
+let body = document.querySelector('body');
+let textChange = document.getElementById('text-change');
+let locationText = document.getElementById('location');
+let teamLogo = document.getElementById('team-logo');
+let topBar = document.querySelector('.top-bar');
+let innerBar = document.querySelector('.inner-bar');
+let polygonTop = document.querySelector('.polygon-top');
+let footerColor = document.querySelector('footer');
+let innerFooter = document.querySelector('.inner-bottom-bar');
+let polygonBottom = document.querySelector('.polygon-bottom');
+
+document.querySelector('.alabama').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/alabama-bg.jpg')";
+        textChange.textContent = "Bryant-Denny Stadium";
+        locationText.textContent = "Tuscaloosa, Alabama";
+        teamLogo.src = 'logos/alabama.png';
+        topBar.style.backgroundColor = "#a60c31";
+        innerBar.style.backgroundColor = "#cccccc";
+        polygonTop.style.backgroundColor = "#cccccc";
+        footerColor.style.backgroundColor = "#cccccc";
+        innerFooter.style.backgroundColor = "#a60c31";
+        polygonBottom.style.backgroundColor = "#cccccc";
+    }
+);
+
+document.querySelector('.arkansas').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/arkansas-bg.jpeg')";
+        textChange.textContent = "Donald W. Reynolds Stadium";
+        locationText.textContent = "Fayetteville, Arkansas";
+        teamLogo.src= 'logos/arkansas.png';
+        topBar.style.backgroundColor = "#9d2235";
+        innerBar.style.backgroundColor = "#000000";
+        polygonTop.style.backgroundColor = "#000000";
+        footerColor.style.backgroundColor = "#000000";
+        innerFooter.style.backgroundColor = "#9d2235";
+        polygonBottom.style.backgroundColor = "#000000";
+    }
+);
+
+document.querySelector('.auburn').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/auburn-bg.jpg')";
+        textChange.textContent = "Jordan-Hare Stadium";
+        locationText.textContent = "Auburn, Alabama";
+        teamLogo.src = 'logos/auburn.png';
+        topBar.style.backgroundColor = "#03244d";
+        innerBar.style.backgroundColor = "#f26552";
+        polygonTop.style.backgroundColor = "#f26552";
+        footerColor.style.backgroundColor = "#f26552";
+        innerFooter.style.backgroundColor = "#03244d";
+        polygonBottom.style.backgroundColor = "#f26552";
+    }
+);
+
+document.querySelector('.lsu').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/lsu-bg.jpg')";
+        textChange.textContent = "Tiger Stadium";
+        locationText.textContent = "Baton Rouge, Louisiana";
+        teamLogo.src = 'logos/lsu.png';
+        topBar.style.backgroundColor = "#461d7c";
+        innerBar.style.backgroundColor = "#fdd023";
+        polygonTop.style.backgroundColor = "#fdd023";
+        footerColor.style.backgroundColor = "#fdd023";
+        innerFooter.style.backgroundColor = "#461d7c";
+        polygonBottom.style.backgroundColor = "#fdd023";
+    }
+);
+
+document.querySelector('.mississippi-state').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/mississippi state-bg.jpg')";
+        textChange.textContent = "Davis Wade Stadium";
+        locationText.textContent = "Mississippi State, Mississippi";
+        teamLogo.src = 'logos/mississippi-state.png';
+        topBar.style.backgroundColor = "#702e3e";
+        innerBar.style.backgroundColor = "#c1c5c8";
+        polygonTop.style.backgroundColor = "#c1c5c8";
+        footerColor.style.backgroundColor = "#c1c5c8";
+        innerFooter.style.backgroundColor = "#702e3e";
+        polygonBottom.style.backgroundColor = "#c1c5c8";
+    }
+);
+
+document.querySelector('.mississippi').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/mississippi-bg.jpg')";
+        textChange.textContent = "Vaught Hemingway Stadium";
+        locationText.textContent = "Oxford, Mississippi";
+        teamLogo.src = 'logos/ole-miss.png';
+        topBar.style.backgroundColor = "#14213d";
+        innerBar.style.backgroundColor = "#c31126";
+        polygonTop.style.backgroundColor = "#c31126";
+        footerColor.style.backgroundColor = "#c31126";
+        innerFooter.style.backgroundColor = "#14213d";
+        polygonBottom.style.backgroundColor = "#c31126";
+    }
+);
+
+document.querySelector('.texas-am').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/texas a&m-bg.jpg')";
+        textChange.textContent = "Kyle Field";
+        locationText.textContent = "College Station, Texas";
+        teamLogo.src = 'logos/texas-am.png';
+        topBar.style.backgroundColor = "#500000";
+        innerBar.style.backgroundColor = "#000000";
+        polygonTop.style.backgroundColor = "#000000";
+        footerColor.style.backgroundColor = "#000000";
+        innerFooter.style.backgroundColor = "#500000";
+        polygonBottom.style.backgroundColor = "#000000";
+    }
+);
+
+document.querySelector('.florida').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/florida-bg.jpeg')";
+        textChange.textContent = "Ben Hill Griffin Stadium";
+        locationText.textContent = "Gainesville, Florida";
+        teamLogo.src = 'logos/florida.png';
+        topBar.style.backgroundColor = "#0021a5";
+        innerBar.style.backgroundColor = "#ff4a00";
+        polygonTop.style.backgroundColor = "#ff4a00";
+        footerColor.style.backgroundColor = "#ff4a00";
+        innerFooter.style.backgroundColor = "#0021a5";
+        polygonBottom.style.backgroundColor = "#ff4a00";
+    }
+);
+
+document.querySelector('.georgia').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/georgia-bg.jpg')";
+        textChange.textContent = "Sanford Stadium";
+        locationText.textContent = "Athens, Georgia";
+        teamLogo.src = 'logos/georgia.png';
+        topBar.style.backgroundColor = "#ba0c2f";
+        innerBar.style.backgroundColor = "#000000";
+        polygonTop.style.backgroundColor = "#000000";
+        footerColor.style.backgroundColor = "#000000";
+        innerFooter.style.backgroundColor = "#ba0c2f";
+        polygonBottom.style.backgroundColor = "#000000";
+    }
+);
+
+document.querySelector('.kentucky').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/kentucky-bg.jpg')";
+        textChange.textContent = "Kroger Field";
+        locationText.textContent = "Lexington, Kentucky";
+        teamLogo.src = 'logos/kentucky.png';
+        topBar.style.backgroundColor = "#0033a0";
+        innerBar.style.backgroundColor = "#000000";
+        polygonTop.style.backgroundColor = "#000000";
+        footerColor.style.backgroundColor = "#000000";
+        innerFooter.style.backgroundColor = "#0033a0";
+        polygonBottom.style.backgroundColor = "#000000";
+    }
+);
+
+document.querySelector('.missouri').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/missouri-bg.jpeg')";
+        textChange.textContent = "Memorial Stadium";
+        locationText.textContent = "Columbia, Missouri";
+        teamLogo.src = 'logos/missouri.png';
+        topBar.style.backgroundColor = "#f1b82d";
+        innerBar.style.backgroundColor = "#000000";
+        polygonTop.style.backgroundColor = "#000000";
+        footerColor.style.backgroundColor = "#000000";
+        innerFooter.style.backgroundColor = "#f1b82d";
+        polygonBottom.style.backgroundColor = "#000000";
+    }
+);
+
+document.querySelector('.south-carolina').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/south carolina-bg.jpg')";
+        textChange.textContent = "Williams Brice Stadium";
+        locationText.textContent = "Columbia, South Carolina";
+        teamLogo.src = 'logos/south-carolina.png';
+        topBar.style.backgroundColor = "#73000a";
+        innerBar.style.backgroundColor = "#000000";
+        polygonTop.style.backgroundColor = "#000000";
+        footerColor.style.backgroundColor = "#000000";
+        innerFooter.style.backgroundColor = "#73000a";
+        polygonBottom.style.backgroundColor = "#000000";
+    }
+);
+
+document.querySelector('.tennessee').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/tennessee-bg.jpg')";
+        textChange.textContent = "Neyland Stadium";
+        locationText.textContent = "Knoxville, Tennessee";
+        teamLogo.src = 'logos/tennessee.png';
+        topBar.style.backgroundColor = "#ff8200";
+        innerBar.style.backgroundColor = "#58595b";
+        polygonTop.style.backgroundColor = "#58595b";
+        footerColor.style.backgroundColor = "#58595b";
+        innerFooter.style.backgroundColor = "#ff8200";
+        polygonBottom.style.backgroundColor = "#58595b";
+    }
+);
+
+document.querySelector('.vanderbilt').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/vanderbilt-bg.jpg')";
+        textChange.textContent = "FirstBank Stadium";
+        locationText.textContent = "Nashville, Tennessee";
+        teamLogo.src = 'logos/vanderbilt.png';
+        topBar.style.backgroundColor = "#ceb888";
+        innerBar.style.backgroundColor = "#000000";
+        polygonTop.style.backgroundColor = "#000000";
+        footerColor.style.backgroundColor = "#000000";
+        innerFooter.style.backgroundColor = "#ceb888";
+        polygonBottom.style.backgroundColor = "#000000";
+    }
+);
+
+document.querySelector('.sec').addEventListener('click', function()
+    {
+        body.style.backgroundImage = "url('img/bg.jpg')";
+        textChange.textContent = "Click on team banner for stadium background";
+        locationText.textContent = "";
+        teamLogo.src='logos/ncaa-logo.png';
+        topBar.style.backgroundColor = "#014c8d";
+        innerBar.style.backgroundColor = "#ffd047";
+        polygonTop.style.backgroundColor = "#ffd047";
+        footerColor.style.backgroundColor = "#ffd047";
+        innerFooter.style.backgroundColor = "#014c8d";
+        polygonBottom.style.backgroundColor = "#ffd047";
+    }
+);
